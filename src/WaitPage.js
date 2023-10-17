3f9cc1e2aeba19b0c673b2a8eab95d6daee43bdb
minales <inamales@gmail.com>
2023-10-18 01:56:52 +0200

    Wait page added

---
+++ src/WaitPage.js
@@ -0,0 +1,27 @@
+import './WaitPage.css';
+
+function WaitPage() {
+    
+    return(
+        <>
+        <h1>Coming soon</h1>
+        <h4>Our website is under construction</h4>
+        <div class="screen">
+            <div class="road">
+                <div class="strip strip1"></div>
+                <div class="strip2 strip"></div>
+                <div class="car-wrap">
+                <div class="tops"></div>
+                <div class="window"></div>
+                <div class="back-window"></div>
+                <div class="car-body"></div>
+                <div class="wheel"></div>
+                <div class="bck-wheel"></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default WaitPage;
